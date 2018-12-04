610364921556afd3f0e54e9a4e3c436220c55038
Hugo Alliaume <kocal@live.fr>
2018-12-04 09:05:03 +0100

    feat: configure environments to `browser` and `es6` (#19)

--- file-to-lint.js
+++ file-to-lint.js
@@ -1,5 +1,28 @@
 const doubleArrayValues = (arr) => arr.map(v => v * 2);
 
 (() => {
+  const set = new Set();
+  const map = new Map();
+
   console.log(doubleArrayValues([1, 2, 3]));
+  console.log(global.location.href);
+
+  const promise = new Promise(resolve => {
+    return fetch('https://example.com')
+      .then(response => {
+        if (!response.ok) {
+          return;
+        }
+
+        return response.json();
+      })
+      .then(json => resolve(json));
+  });
+
+  promise.then(json => {
+    set.add('foo');
+    set.add('bar');
+    map.set('first_value', 123);
+    map.set('json', json);
+  });
 })();
